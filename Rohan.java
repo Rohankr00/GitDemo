6f12f24065c9f6a71969ad36e4dec3c8df98fa01
Rohan Kumar <rohansharma05062005@gmail.com>
2026-01-04 18:51:52 +0530

    After Clonnig

--- Rohan.java
+++ Rohan.java
@@ -3,5 +3,9 @@ class Rohan {
         byte a=5,b=8;
         System.out.print("Add: "+(a+b));
         
+
+        System.out.print("\nSub: "+(a-b));
+        System.out.print("\nMul: "+(a*b));
+        System.out.print("\nDiv: "+(a/b));
     }
 }
